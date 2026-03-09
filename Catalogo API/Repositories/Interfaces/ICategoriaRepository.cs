541f9d6684079879b6002a21ce1bae6488c5f98f
felipeolv11 <felipeeh1524@gmail.com>
2026-03-09 16:04:28 -0300

    adicionar paginação nas interfaces de repositório de categoria e produto

--- Catalogo API/Repositories/Interfaces/ICategoriaRepository.cs
+++ Catalogo API/Repositories/Interfaces/ICategoriaRepository.cs
@@ -1,7 +1,9 @@
 ﻿using Catalogo_API.Models;
+using Catalogo_API.Pagination;
 
 namespace Catalogo_API.Repositories.Interfaces;
 
 public interface ICategoriaRepository : IRepository<Categoria>
 {
+    PagedList<Categoria> GetCategorias(CategoriasParameters categoriasParams);
 }
--- Catalogo API/Repositories/Interfaces/IProdutoRepository.cs
+++ Catalogo API/Repositories/Interfaces/IProdutoRepository.cs
@@ -1,8 +1,10 @@
 ﻿using Catalogo_API.Models;
+using Catalogo_API.Pagination;
 
 namespace Catalogo_API.Repositories.Interfaces;
 
 public interface IProdutoRepository : IRepository<Produto>
 {
+    PagedList<Produto> GetProdutos(ProdutosParameters produtosParams);
     IEnumerable<Produto> GetProdutosPorCategoria(int id);
 }
