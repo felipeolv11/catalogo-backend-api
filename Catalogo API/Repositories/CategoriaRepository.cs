4a6b0e6fdb4ea25a1024636f6dd7d067b9459a74
felipeolv11 <felipeeh1524@gmail.com>
2026-03-09 16:06:59 -0300

    adicionar paginação nos repositórios de categoria e produto

--- Catalogo API/Repositories/CategoriaRepository.cs
+++ Catalogo API/Repositories/CategoriaRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Catalogo_API.Context;
 using Catalogo_API.Models;
+using Catalogo_API.Pagination;
 using Catalogo_API.Repositories.Interfaces;
 
 namespace Catalogo_API.Repositories;
@@ -8,5 +9,15 @@ public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
 {
     public CategoriaRepository(CatalogoDbContext context) : base(context)
     {
+    }
+
+    public PagedList<Categoria> GetCategorias(CategoriasParameters categoriaParameters)
+    {
+        var categorias = GetAll().OrderBy(c => c.CategoriaId).AsQueryable();
+
+        var categoriasOrdenadas = PagedList<Categoria>.ToPagedList(categorias, 
+            categoriaParameters.PageNumber, categoriaParameters.PageSize);
+
+        return categoriasOrdenadas;
     }
 }
--- Catalogo API/Repositories/ProdutoRepository.cs
+++ Catalogo API/Repositories/ProdutoRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Catalogo_API.Context;
 using Catalogo_API.Models;
+using Catalogo_API.Pagination;
 using Catalogo_API.Repositories.Interfaces;
 
 namespace Catalogo_API.Repositories;
@@ -8,6 +9,16 @@ public class ProdutoRepository : Repository<Produto>, IProdutoRepository
 {
     public ProdutoRepository(CatalogoDbContext context) : base(context)
     {
+    }
+
+    public PagedList<Produto> GetProdutos(ProdutosParameters produtoParams)
+    {
+        var produtos = GetAll().OrderBy(p => p.ProdutoId).AsQueryable();
+
+        var produtosOrdenados = PagedList<Produto>.ToPagedList(produtos,
+            produtoParams.PageNumber, produtoParams.PageSize);
+
+        return produtosOrdenados;
     }
 
     public IEnumerable<Produto> GetProdutosPorCategoria(int id)
